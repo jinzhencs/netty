f97856405a339982a8304e074eda8c697590490b
jinzhencs <chenchen.ming@56qq.com>
2017-09-22 00:35:36 +0800

    feat: add logback

--- demo/src/main/java/com/mcc/learn/netty/demo/Main.java
+++ demo/src/main/java/com/mcc/learn/netty/demo/Main.java
@@ -2,6 +2,8 @@ package com.mcc.learn.netty.demo;
 
 import com.mcc.learn.netty.demo.client.HelloNettyClient;
 import com.mcc.learn.netty.demo.server.HelloNettyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,7 +12,9 @@ import java.util.concurrent.Executors;
  * Created by Mingchenchen on 2017/9/20
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void startAll() throws Exception {
         int port = 8080;
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
@@ -23,5 +27,15 @@ public class Main {
         HelloNettyClient client = new HelloNettyClient();
         client.connect("127.0.0.1", port);
         //浏览器访问http://localhost:8080 即可看到输出
+    }
+
+    public static void main(String[] args) throws Exception {
+        logger.debug("debug");
+        logger.trace("trace");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.info("test: {}", "bbb");
+        //startAll();
     }
 }
